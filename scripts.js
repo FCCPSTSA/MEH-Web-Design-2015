b63db9d31c8993bfa14d91bdbed1a86de9084163
Aloup Shmederheisen <Aardwolf95@users.noreply.github.com>
2015-11-26 14:52:22 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,24 @@
+var isOpen = false;
+
 $(document).ready(function () {
 	$('.menu-button').click(function () {
 		$(this).toggleClass('open');
 	});
 });
-$(document).ready(function () {
-	$('.menu-button').click(function () {
-		var isOpened = true;
-		$('.menu').animate({
+if (isOpen === false) {
+	$(document).ready(function () {
+		$('.menu-button').click(function () {
+			isOpened = true;
+			$('.menu').animate({
 			width: "30vw"
+			});
 		});
 	});
-});
+}
 if (isOpened === true) {
 	$(document).ready(function () {
 		$('.menu-button').click(function () {
+			isOpen = false
 			$('.menu').animate({
 				width: "0vw"
 			});
