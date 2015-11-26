7109f361f65a607e2acf27d5a4c60b1aab2c4bee
Aloup Shmederheisen <Aardwolf95@users.noreply.github.com>
2015-11-26 14:46:30 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,8 +5,18 @@ $(document).ready(function () {
 });
 $(document).ready(function () {
 	$('.menu-button').click(function () {
+		var isOpened = true;
 		$('.menu').animate({
-			width: "30vw"	
+			width: "30vw"
 		});
 	});
 });
+if (isOpened === true) {
+	$(document).ready(function () {
+		$('.menu-button').click(function () {
+			$('.menu').animate({
+				width: "0vw"
+			});
+		});
+	});
+}
