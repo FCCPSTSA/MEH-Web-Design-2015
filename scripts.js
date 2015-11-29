0cec70ba09810d425ee89179259dc1a439a2d68f
DarkReaperRising <jamestrombo@gmail.com>
2015-11-29 18:36:47 -0500

    Fixified
    Call Me Fix It Felix. I put the things to work again with paulo’s color
    scheme.

--- scripts.js
+++ scripts.js
@@ -1,27 +1,11 @@
-var isOpen = false;
-
-var open = function() {
-    $(".menu").animate({
-        width: "30vw"
-    }, 300);
-
-    isOpen = true;
-};
-var menuClose = function() {
-    $(".menu").animate({
-        width: "0vw"
-    }, 300);
-    isOpen = false;
-};
-var menu = function() {
-    $(".menu-button").click(function() {
-        if (isMenuOpen === false) {
-            menuOpen();
-        }
-        else {
-            menuClose();
-        }
+$(document).ready(function () {
+    $('.nav-button').click(function () {
+        $(this).toggleClass('open');
     });
-};
+});
 
-$(document).ready(menu);
+$(document).ready(function () {
+    $('.nav-button').click(function () {
+        $('.thingofmenu').toggleClass('opened');
+    });
+});
