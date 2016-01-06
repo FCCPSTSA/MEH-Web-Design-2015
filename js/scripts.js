d3f26a090b696a75307d3349387158f40b56dff6
DarkReaperRising <jamestrombo@gmail.com>
2016-01-06 11:45:50 -0500

    Rudimentary Proof of Concept

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,14 @@ $(document).ready(function() {
         $('.menu').toggleClass('open');
     });
 });
+$(document).on('scroll', function(e) {
+    var value = $(this).scrollTop();
+    if ( value < 100 ) {
+        $("header").css("height", "100px");
+        $("h1").css("font-size", "75px");
+    }
+    else {
+        $("header").css("height", "45px");
+        $("h1").css("font-size", "30px");
+    }
+});
