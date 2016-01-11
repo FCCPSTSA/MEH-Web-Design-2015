382d43c931634ac2424ca2d292c7c7aa1ae858e1
DarkReaperRising <jamestrombo@gmail.com>
2016-01-11 07:52:42 -0500

    Eye I got it on my macbook
    I should get a server computer lmao

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+$(document).on('scroll', function(e) {
+    var value = $(this).scrollTop();
+    if ( value < 200 ) {
+        $("header").css("height", "100px");
+        $('h1').css({'font-size' : '75px'});
+    }
+    else {
+        $("header").css("height", "60px");
+        $('h1').css({'font-size' : '30px'});
+    }
+});
 $(document).ready(function() {
     $('.nav-button').click(function() {
         $(this).toggleClass('open');
@@ -7,15 +18,4 @@ $(document).ready(function() {
     $('.nav-button').click(function() {
         $('.menu').toggleClass('open');
     });
-});
-$(document).on('scroll', function(e) {
-    var value = $(this).scrollTop();
-    if ( value < 100 ) {
-        $("header").css("height", "100px");
-        $("h1").css("font-size", "75px");
-    }
-    else {
-        $("header").css("height", "45px");
-        $("h1").css("font-size", "30px");
-    }
 });
