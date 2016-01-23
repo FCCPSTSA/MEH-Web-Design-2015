104ad43f3a8f3c1a0dd6619e3607e1694d63d279
DarkReaperRising <jamestrombo@gmail.com>
2016-01-23 16:00:43 -0500

    Like it?

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 $(document).on('scroll', function(e) {
     var value = $(this).scrollTop();
     if ( value < 200 ) {
-        $("header").css("height", "100px");
+        $('header').css({'height' : '100px'});
         $('h1').css({'font-size' : '75px'});
+        $('.nav-button').css({'visibility' : 'hidden'});
+        $('.toplinks').css({'visibility' : 'visible'});
     }
     else {
-        $("header").css("height", "60px");
+        $('header').css({'height' : '60px'});
         $('h1').css({'font-size' : '30px'});
+        $('.nav-button').css({'visibility' : 'visible'});
+        $('.toplinks').css({'visibility' : 'hidden'});
     }
 });
 $(document).ready(function() {
     $('.nav-button').click(function() {
         $(this).toggleClass('open');
-    });
-});
-$(document).ready(function() {
-    $('.nav-button').click(function() {
         $('.menu').toggleClass('open');
     });
 });
