9a9ca678cbe3b79b7e644686745df2ed288d3fee
Aardwolf95 <ashmederheisen@gmail.com>
2016-05-03 22:11:09 -0400

    Moving to my good computer before I finish my changes.

--- js/energy.js
+++ js/energy.js
@@ -1,21 +1,21 @@
 var contentplace = document.getElementsByClassName('content');
-for ( i=0; i<contentplace.length; i++) {
-	var randnum = Math.floor(Math.random()*100);
-	var orig = randnum;
-	if (randnum<35) {
-		randnum = randnum+35;
-	}
-	if (randnum>65) {
-		randnum = randnum-35;
-	}
-	if (orig>48) {
-		while (orig>48) {
-			orig--;
-		}
-	}
-	if (orig<10) {
-		orig = orig+10;
-	}
-	contentplace[i].style.left = randnum + 'vw';
-	contentplace[i].style.top = orig + 'vh';
+for (i = 0; i < contentplace.length; i++) {
+  var randnum = Math.floor(Math.random() * 80);
+  var orig = randnum;
+  if (randnum < 35) {
+    randnum = randnum + 35;
+  }
+  if (randnum > 65) {
+    randnum = randnum - 35;
+  }
+  if (orig > 48) {
+    while (orig > 48) {
+      orig--;
+    }
+  }
+  if (orig < 10) {
+    orig = orig + 10;
+  }
+  contentplace[i].style.left = randnum + 'vw';
+  contentplace[i].style.top = orig + 'vh';
 }
