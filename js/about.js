5f2421ff4a6496644e0da8376fab287db31d3382
DarkReaperRising <jamestrombo@gmail.com>
2016-06-17 12:11:35 -0400

    Blood of 71 virgins

--- js/about.js
+++ js/about.js
@@ -1,7 +1,10 @@
 previouspage = localStorage.lastpage;
 bar = document.getElementById('whereyouat');
 console.log(localStorage.lastpage)
-
+var toplinks = {
+    home: document.getElementById('home'),
+    history: document.getElementById('history'),
+};
 function toggleClass(name) {
     bar.className = name;
 }
@@ -12,7 +15,7 @@ if (previouspage == 'history') {
     }, 200);
 }
 if (previouspage == 'about') {
-    bar.style.left = '1100px';
+    bar.style.left = '885px';
 }
 if (previouspage == 'home') {
     bar.style.left = '562px';
@@ -21,6 +24,33 @@ if (previouspage == 'home') {
     }, 200);
 }
 if (previouspage == '') {
-    bar.style.left = '1100px';
+    bar.style.left = '885px';
 }
 localStorage.setItem('lastpage', 'about')
+i = 0;
+while (i < 2) {
+    item = document.createElement('div');
+    item.className = 'hoverbar';
+    document.body.appendChild(item);
+    i++;
+    console.log('yo');
+}
+hoverbars = document.getElementsByClassName('hoverbar');
+console.log(hoverbars);
+positions = ['712px', '562px'];
+for (i = 0; i < hoverbars.length; i++) {
+    hoverbars[i].style.left = positions[i];
+    hoverbars[i].style.opacity = '0';
+}
+toplinks.history.onmouseover = function() {
+    hoverbars[0].style.opacity = '0.75';
+}
+toplinks.history.onmouseout = function() {
+    hoverbars[0].style.opacity = '0';
+}
+toplinks.home.onmouseover = function() {
+    hoverbars[1].style.opacity = '0.75';
+}
+toplinks.home.onmouseout = function() {
+    hoverbars[1].style.opacity = '0';
+}
